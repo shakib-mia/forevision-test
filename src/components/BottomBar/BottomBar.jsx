43b0ad8aa67d6650e719fd07722b9847c34bc44a
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-11-02 19:54:29 +0600

    bottom bar done

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { navItem } from "../../constants";
+import { navPhone } from "../../constants";
+// import { nav } from "../../constants";
+import more from './../../assets/icons/more.webp'
+import { NavLink } from "react-router-dom";
 
 const BottomBar = () => {
+  // alert(navPhone.length)
   return (
-    <nav className="fixed bottom-0 shadow w-screen py-3 bg-white flex justify-center gap-3 lg:hidden">
-      {navItem.slice(0, navItem.length - 1).map(({ icon, text }, key) => (
-        <img src={icon} alt={text} key={key} />
+    <nav className="fixed bottom-0 z-[99999] px-1 shadow w-screen bg-white flex justify-center lg:hidden" id="bottomBar">
+      {navPhone.slice(0, navPhone.length).map(({ icon, text, path }, key) => (
+        <NavLink to={path} className="w-1/5 flex justify-center flex-col items-center px-1 py-1 gap-[4px]" key={key}>
+          {icon}
+          <h2 className="text-paragraph-2">{text}</h2>
+        </NavLink>
       ))}
+
+      <div className="w-1/5 flex justify-center flex-col items-center px-1 gap-[4px]">
+        <img src={more} className="w-3 h-3" alt={'text'} key={'key'} />
+        <h2 className="text-paragraph-2">{'More'}</h2>
+      </div>
     </nav>
   );
 };
