b7c8c103c937b7b70f0a82be6572c8e15871ed7e
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-12-03 18:25:11 +0600

    revenue details issue fixed

--- src/components/RevenueDetails/RevenueDetails.jsx
+++ src/components/RevenueDetails/RevenueDetails.jsx
@@ -4,8 +4,10 @@ const RevenueDetails = ({ item, raw, detailsRef }) => {
     // if (raw) {
     const items = raw?.filter(i => i.music_isrc === item.music_isrc)
     // console.log(items);
-    // items.map(i => console.log(i.music_isrc))
+    // items?.map(i => console.log(i.forevision_cut))
     // }
+
+
 
 
     return (
@@ -21,7 +23,7 @@ const RevenueDetails = ({ item, raw, detailsRef }) => {
                     <h6 className='py-2 order-1 2xl:order-none 2xl:text-paragraph-2 2xl:text-grey-dark font-normal 2xl:text-center'>{i.music_isrc ? i.music_isrc : '-'}</h6>
                     <h6 className='py-2 hidden 2xl:block 2xl:text-paragraph-2 2xl:text-grey-dark font-normal 2xl:text-center'>{i.music_royality ? i.music_royality : '-'}</h6>
                     <h6 className='py-2 hidden 2xl:block 2xl:text-paragraph-2 2xl:text-grey-dark font-normal 2xl:text-center'>{i.music_after_tds_revenue ? i.music_after_tds_revenue : '-'}</h6>
-                    <h6 className='py-2 hidden 2xl:block 2xl:text-paragraph-2 2xl:text-grey-dark font-normal 2xl:text-center'>{i.music_after_tds_revenue ? parseFloat(i.music_after_tds_revenue) - parseFloat(i.music_after_tds_revenue) * (parseFloat(i.forevision_cut) / 100) : '-'}</h6>
+                    <h6 className='py-2 hidden 2xl:block 2xl:text-paragraph-2 2xl:text-grey-dark font-normal 2xl:text-center'>{i.music_after_tds_revenue && i.forevision_cut !== null ? parseFloat(i.music_after_tds_revenue) - parseFloat(i.music_after_tds_revenue) * (parseFloat(i.forevision_cut) / 100) : '-'}</h6>
                 </>)}
             </div>
         </>
