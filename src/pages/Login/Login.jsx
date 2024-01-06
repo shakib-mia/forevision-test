37947355f5f3d48146dd35514490c86871ce585b
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-01-06 12:50:40 +0600

    login page regained

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import AuthBody from "../../components/AuthBody/AuthBody";
 import InputField from "../../components/InputField/InputField";
 import Button from "../../components/Button/Button";
@@ -8,7 +8,6 @@ import { ProfileContext } from "../../contexts/ProfileContext";
 import { toast } from "react-toastify";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import caution from "./../../assets/icons/caution.svg"
-import Construction from "../Construction/Construction";
 
 const Login = () => {
   const [email, setEmail] = useState("")
@@ -92,51 +91,50 @@ const Login = () => {
   };
 
   return (
-    // <AuthBody
-    //   heading="Login"
-    //   altDescription="New in ForeVision Digital?"
-    //   altText="Sign Up"
-    //   altLink="/signup"
-    //   onSubmit={login}
-    // >
-    //   {fields.map((props, id) => (
-    //     <InputField {...props} key={id} containerClassName="mt-3" onChange={e => props.name === 'email' ? setEmail(e.target.value) : setPassword(e.target.value)} />
-    //   ))}
+    <AuthBody
+      heading="Login"
+      altDescription="New in ForeVision Digital?"
+      altText="Sign Up"
+      altLink="/signup"
+      onSubmit={login}
+    >
+      {fields.map((props, id) => (
+        <InputField {...props} key={id} containerClassName="mt-3" onChange={e => props.name === 'email' ? setEmail(e.target.value) : setPassword(e.target.value)} />
+      ))}
 
-    //   <div className="mt-3 mb-2 text-center">
-    //     <Button type="submit" text={loading ? 'Logging in' : "Login"} disabled={loading || !email.length || !password.length} />
-    //   </div>
+      <div className="mt-3 mb-2 text-center">
+        <Button type="submit" text={loading ? 'Logging in' : "Login"} disabled={loading || !email.length || !password.length} />
+      </div>
 
-    //   <div className="text-center">
-    //     <Link
-    //       to="/forgot-password"
-    //       className="text-interactive-light text-button uppercase"
-    //     >
-    //       Forgot your password?
-    //     </Link>
-    //   </div>
+      <div className="text-center">
+        <Link
+          to="/forgot-password"
+          className="text-interactive-light text-button uppercase"
+        >
+          Forgot your password?
+        </Link>
+      </div>
 
-    //   {showMessage ? <div className="fixed left-0 top-0 backdrop-blur w-screen h-screen flex justify-center items-center">
-    //     <div className="w-11/12 xl:w-1/2 bg-white p-5 rounded shadow-xl flex flex-col justify-center items-center gap-2">
+      {showMessage ? <div className="fixed left-0 top-0 backdrop-blur w-screen h-screen flex justify-center items-center">
+        <div className="w-11/12 xl:w-1/2 bg-white p-5 rounded shadow-xl flex flex-col justify-center items-center gap-2">
 
-    //       <img src={caution} className="w-1/3" alt="caution" />
-    //       <p className="text-interactive-light-destructive-focus text-heading-6 text-center justify-center items-center"> It may Take upto <b>Ten(10)</b> minutes to load your data. Be Patient.</p>
-    //       <p className="w-full xl:w-9/12 mx-auto text-center text-paragraph-1 text-grey-dark flex">Check out our new music earnings system! If you hit a snag, let us know, and we'll fix it in around a week. Thanks!</p>
+          <img src={caution} className="w-1/3" alt="caution" />
+          <p className="text-interactive-light-destructive-focus text-heading-6 text-center justify-center items-center"> It may Take upto <b>Ten(10)</b> minutes to load your data. Be Patient.</p>
+          <p className="w-full xl:w-9/12 mx-auto text-center text-paragraph-1 text-grey-dark flex">Check out our new music earnings system! If you hit a snag, let us know, and we'll fix it in around a week. Thanks!</p>
 
-    //       <CountdownCircleTimer
-    //         isPlaying
-    //         duration={10}
-    //         colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-    //         colorsTime={[7, 5, 2, 0]}
-    //         strokeWidth={8}
-    //         size={100}
-    //       >
-    //         {({ remainingTime }) => <span className="text-heading-4-bold text-grey-dark">{remainingTime}</span>}
-    //       </CountdownCircleTimer>
-    //     </div>
-    //   </div> : <></>}
-    // </AuthBody>
-    <Construction />
+          <CountdownCircleTimer
+            isPlaying
+            duration={10}
+            colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+            colorsTime={[7, 5, 2, 0]}
+            strokeWidth={8}
+            size={100}
+          >
+            {({ remainingTime }) => <span className="text-heading-4-bold text-grey-dark">{remainingTime}</span>}
+          </CountdownCircleTimer>
+        </div>
+      </div> : <></>}
+    </AuthBody>
   );
 };
 
