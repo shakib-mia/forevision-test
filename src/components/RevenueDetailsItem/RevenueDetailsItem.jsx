c11dea4efd8ee8ae3a063b0dc9336bb7c7141f17
tanvir <tanvirulislam149@gmail.com>
2024-02-11 02:01:19 +0600

    performance issue fixed

--- src/components/RevenueDetailsItem/RevenueDetailsItem.jsx
+++ src/components/RevenueDetailsItem/RevenueDetailsItem.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const RevenueDetailsItem = ({ options, item }) => {
 
 
-    return (
-        <ul className='grid grid-cols-5 gap-3 text-center mt-2'>
-            {options.map((i, key) => <li>{item[i]}</li>)}
-        </ul>
-    );
+  return (
+    <ul className='grid grid-cols-5 gap-3 text-center mt-2'>
+      {options.map((i, key) => <li>{item[i]}</li>)}
+    </ul>
+  );
 };
 
 export default RevenueDetailsItem;
