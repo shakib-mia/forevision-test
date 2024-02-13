aa80181d5bdf18241fb3e1a4725d2a1015027744
tanvir <tanvirulislam149@gmail.com>
2024-02-13 23:13:27 +0600

    stream bug fixed

--- src/components/RevenueDetailsItem/RevenueDetailsItem.jsx
+++ src/components/RevenueDetailsItem/RevenueDetailsItem.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import MonthStreamCount from '../MonthStreamCount/MonthStreamCount';
 
 const RevenueDetailsItem = ({ result, details, songs }) => {
   const [platform, setPlatform] = useState("");
@@ -36,35 +37,7 @@ const RevenueDetailsItem = ({ result, details, songs }) => {
     <table className='w-full'>
       {
         result.map(i =>
-          <details onClick={() => setPlatform(i.platformName)} className="group">
-            <summary className="cursor-pointer list-none">
-              <tr className='text-center flex justify-between items-center p-1 hover:bg-grey-light rounded-2xl'>
-                <td className='w-1/3 flex items-center ml-3'>
-                  <span className="mr-2 transition group-open:rotate-180 group-open:text-interactive-light-destructive-focus">
-                    <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
-                    </svg>
-                  </span>
-                  {i.platformName}</td>
-                <td className='w-1/3'>{i.total}</td>
-                <td className='w-1/3'>{i["final revenue"]}</td>
-              </tr>
-            </summary>
-            <div className='w-full flex justify-end'>
-              <table className='w-11/12 text-center my-1'>
-                {
-                  countByMonth.map(d =>
-                    <tr style={{ color: "#78B0FD" }}>
-                      <td className='w-1/3'>{
-                        months[new Date(d.date).getMonth()]
-                      } {new Date(d.date).getFullYear()}</td>
-                      <td className='w-1/3'>{d.count}</td>
-                      <td className='w-1/3'>{d.revenue}</td>
-                    </tr>
-                  )
-                }
-              </table>
-            </div>
-          </details>
+          <MonthStreamCount songs={songs} details={details} i={i} />
         )
       }
     </table>
