521801db447c73c95f9c93ef540e664a8001d516
tanvir <tanvirulislam149@gmail.com>
2024-02-13 18:10:33 +0600

    foreign modal

--- src/components/ForeignerModal/ForeignerModal.js
+++ src/components/ForeignerModal/ForeignerModal.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const ForeignerModal = ({ setShowForeignerModal }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
     <div className='w-screen h-screen fixed top-0 left-0 z-[9999] flex justify-center items-center backdrop-blur-[1px]'>
       <div className='md:w-3/5 w-11/12 h-[80vh] relative overflow-x-visible rounded-2xl overflow-y-auto p-3 px-5' style={{ backgroundColor: '#ffffffd9' }}>
@@ -13,34 +16,78 @@ const ForeignerModal = ({ setShowForeignerModal }) => {
           <p className='md:ml-3 mb-3 text-heading-6-bold title text-grey-dark my-1'>Revenue Withdraw foreigner</p>
           <p style={{ fontSize: "21.33px", lineheight: "24px" }} className='md:ml-3 text-heading-6-bold text-grey-dark'>Please fill the form out to initiate the request</p>
         </div>
-        {/* <form onSubmit={handleSubmit} className='mt-5'>
+        <p style={{ color: "#78B0FD" }} className='text-heading-6 md:ml-3 my-2 mt-5'>Submit the Documents needed</p>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div className='md:grid grid-cols-2'>
             <div className='my-4'>
-              <select onChange={(e) => setIsIndian(e.target.value)} id="countries" class="bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2">
-                <option selected>Select</option>
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-              <label for="countries" class="block text-sm text-black mt-1">Are you an Indian citizen?</label>
+              <div className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2'>
+                <label for="files" className="btn flex justify-between items-center">
+                  {/* <p className='overflow-hidden mr-2 h-3'>{!fileName ? "File name" : fileName.split("\\")[2]}</p> */}
+                  <p>filename</p>
+                  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M6.52858 0.528514C6.78892 0.268165 7.21103 0.268165 7.47138 0.528514L10.8047 3.86185C11.0651 4.1222 11.0651 4.54431 10.8047 4.80466C10.5444 5.06501 10.1223 5.06501 9.86191 4.80466L7.66665 2.60939V8.99992C7.66665 9.36811 7.36817 9.66659 6.99998 9.66659C6.63179 9.66659 6.33331 9.36811 6.33331 8.99992V2.60939L4.13805 4.80466C3.8777 5.06501 3.45559 5.06501 3.19524 4.80466C2.93489 4.54431 2.93489 4.1222 3.19524 3.86185L6.52858 0.528514ZM0.99998 8.33325C1.36817 8.33325 1.66665 8.63173 1.66665 8.99992V11.6666C1.66665 11.8434 1.73688 12.013 1.86191 12.138C1.98693 12.263 2.1565 12.3333 2.33331 12.3333H11.6666C11.8435 12.3333 12.013 12.263 12.1381 12.138C12.2631 12.013 12.3333 11.8434 12.3333 11.6666V8.99992C12.3333 8.63173 12.6318 8.33325 13 8.33325C13.3682 8.33325 13.6666 8.63173 13.6666 8.99992V11.6666C13.6666 12.197 13.4559 12.7057 13.0809 13.0808C12.7058 13.4559 12.1971 13.6666 11.6666 13.6666H2.33331C1.80288 13.6666 1.29417 13.4559 0.919099 13.0808C0.544027 12.7057 0.333313 12.197 0.333313 11.6666V8.99992C0.333313 8.63173 0.63179 8.33325 0.99998 8.33325Z" fill="#202020" fill-opacity="0.8" />
+                  </svg>
+                </label>
+                <input type="file" name="gstCertificate" id="files" style={{ display: "none" }} />
+              </div>
+              <label for="countries" class="block text-sm text-black mt-1">Upload your Government ID</label>
             </div>
             <div className='my-4'>
-              <select id="countries" class="bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2" onChange={(e) => setHaveGST(e.target.value)}>
-                <option selected>Select</option>
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-              <label for="countries" class="block text-sm text-black mt-1">Do you have a GST number?</label>
+              <div className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2'>
+                <label for="files" className="btn flex justify-between items-center">
+                  {/* <p className='overflow-hidden mr-2 h-3'>{!fileName ? "File name" : fileName.split("\\")[2]}</p> */}
+                  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M6.52858 0.528514C6.78892 0.268165 7.21103 0.268165 7.47138 0.528514L10.8047 3.86185C11.0651 4.1222 11.0651 4.54431 10.8047 4.80466C10.5444 5.06501 10.1223 5.06501 9.86191 4.80466L7.66665 2.60939V8.99992C7.66665 9.36811 7.36817 9.66659 6.99998 9.66659C6.63179 9.66659 6.33331 9.36811 6.33331 8.99992V2.60939L4.13805 4.80466C3.8777 5.06501 3.45559 5.06501 3.19524 4.80466C2.93489 4.54431 2.93489 4.1222 3.19524 3.86185L6.52858 0.528514ZM0.99998 8.33325C1.36817 8.33325 1.66665 8.63173 1.66665 8.99992V11.6666C1.66665 11.8434 1.73688 12.013 1.86191 12.138C1.98693 12.263 2.1565 12.3333 2.33331 12.3333H11.6666C11.8435 12.3333 12.013 12.263 12.1381 12.138C12.2631 12.013 12.3333 11.8434 12.3333 11.6666V8.99992C12.3333 8.63173 12.6318 8.33325 13 8.33325C13.3682 8.33325 13.6666 8.63173 13.6666 8.99992V11.6666C13.6666 12.197 13.4559 12.7057 13.0809 13.0808C12.7058 13.4559 12.1971 13.6666 11.6666 13.6666H2.33331C1.80288 13.6666 1.29417 13.4559 0.919099 13.0808C0.544027 12.7057 0.333313 12.197 0.333313 11.6666V8.99992C0.333313 8.63173 0.63179 8.33325 0.99998 8.33325Z" fill="#202020" fill-opacity="0.8" />
+                  </svg>
+                </label>
+                <input type="file" name="gstCertificate" id="files" style={{ display: "none" }} />
+              </div>
+              <label for="countries" class="block text-sm text-black mt-1">Upload your Secondary Unique ID</label>
             </div>
             <div className='my-4'>
-              <input type="number" value={amount === 0 ? "" : amount} onChange={(e) => setAmount(e.target.value)} className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2' placeholder='&#8377;' name="amount" id="" />
-              <label for="countries" class="block text-sm text-black mt-1">Enter amount</label>
+              <div className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2'>
+                <label for="files" className="btn flex justify-between items-center">
+                  {/* <p className='overflow-hidden mr-2 h-3'>{!fileName ? "File name" : fileName.split("\\")[2]}</p> */}
+                  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M6.52858 0.528514C6.78892 0.268165 7.21103 0.268165 7.47138 0.528514L10.8047 3.86185C11.0651 4.1222 11.0651 4.54431 10.8047 4.80466C10.5444 5.06501 10.1223 5.06501 9.86191 4.80466L7.66665 2.60939V8.99992C7.66665 9.36811 7.36817 9.66659 6.99998 9.66659C6.63179 9.66659 6.33331 9.36811 6.33331 8.99992V2.60939L4.13805 4.80466C3.8777 5.06501 3.45559 5.06501 3.19524 4.80466C2.93489 4.54431 2.93489 4.1222 3.19524 3.86185L6.52858 0.528514ZM0.99998 8.33325C1.36817 8.33325 1.66665 8.63173 1.66665 8.99992V11.6666C1.66665 11.8434 1.73688 12.013 1.86191 12.138C1.98693 12.263 2.1565 12.3333 2.33331 12.3333H11.6666C11.8435 12.3333 12.013 12.263 12.1381 12.138C12.2631 12.013 12.3333 11.8434 12.3333 11.6666V8.99992C12.3333 8.63173 12.6318 8.33325 13 8.33325C13.3682 8.33325 13.6666 8.63173 13.6666 8.99992V11.6666C13.6666 12.197 13.4559 12.7057 13.0809 13.0808C12.7058 13.4559 12.1971 13.6666 11.6666 13.6666H2.33331C1.80288 13.6666 1.29417 13.4559 0.919099 13.0808C0.544027 12.7057 0.333313 12.197 0.333313 11.6666V8.99992C0.333313 8.63173 0.63179 8.33325 0.99998 8.33325Z" fill="#202020" fill-opacity="0.8" />
+                  </svg>
+                </label>
+                <input type="file" name="gstCertificate" id="files" style={{ display: "none" }} />
+              </div>
+              <label for="countries" class="block text-sm text-black mt-1">Upload a photograph of your cancelled Cheque</label>
             </div>
-            <div className='my-4 block'>
-              <button className='disabled:bg-interactive-light-disabled disabled:cursor-not-allowed text-white bg-interactive-light text-button hover:bg-interactive-light-hover focus:bg-interactive-light-focus active:bg-interactive-light-active font-bold rounded-full uppercase flex gap-1 px-3 py-2'>Save and Next</button>
-            </div>
-
           </div>
-        </form> */}
+          <p style={{ color: "#78B0FD" }} className='text-heading-6 md:ml-3 my-2 mt-5'>Submit your Bank Details</p>
+          <div className='grid grid-cols-2'>
+            <div className='my-4'>
+              <input type="text" className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2' placeholder='Account Number' name="accountNumber" id="" />
+              <label for="countries" class="block text-sm text-black mt-1">Enter your Account number</label>
+            </div>
+            <div className='my-4'>
+              <input type="text" className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2' placeholder='Re-enter Account Number' name="accountNumberCheck" id="" />
+              <label for="countries" class="block text-sm text-black mt-1">Confirm your Account number</label>
+            </div>
+            <div className='my-4'>
+              <input type="text" className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2' placeholder='Enter Your Name' name="name" id="" />
+              <label for="countries" class="block text-sm text-black mt-1">Name as per your Bank Account </label>
+            </div>
+            <div className='my-4'>
+              <input type="number" className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2' placeholder='IFSC Code' name="ifsc" id="" />
+              <label for="countries" class="block text-sm text-black mt-1">Enter your IFSC Code</label>
+            </div>
+            <div className='my-4'>
+              <input type="text" className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2' placeholder="Bank's name" name="bankName" id="" />
+              <label for="countries" class="block text-sm text-black mt-1">Your Bank Name</label>
+            </div>
+            <div className='my-4'>
+              <input type="text" className='bg-gray-50 text-black text-sm shadow-md rounded-lg block md:w-5/6 w-full p-2' placeholder='Branch Name' name="branchName" id="" />
+              <label for="countries" class="block text-sm text-black mt-1">Bank's branch Name</label>
+            </div>
+          </div>
+          <div className='my-4 flex justify-center'>
+            <input type='submit' value={"SAVE AND SUBMIT"} className='disabled:bg-interactive-light-disabled disabled:cursor-not-allowed text-white bg-interactive-light text-button hover:bg-interactive-light-hover focus:bg-interactive-light-focus active:bg-interactive-light-active font-bold rounded-full uppercase flex gap-1 px-3 py-2' />
+          </div>
+        </form>
       </div>
     </div>
   )
