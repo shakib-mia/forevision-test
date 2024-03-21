7d782c61eff713562b98728cfce21cab717c22e5
TarekSolaiman <mdtareksolaiman16@gmail.com>
2024-03-21 22:39:51 +0600

    create a new Page for revenue form

--- src/pages/RevenueFrom/Reveneue.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Reveneue() {
-  return <div>Reveneue</div>;
-}
-
-export default Reveneue;
---
+++ src/pages/RevenueFrom/RevenueFrom.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RevenueFrom() {
+  return <div>RevenueFrom</div>;
+}
+
+export default RevenueFrom;
