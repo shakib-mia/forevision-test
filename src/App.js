fb9585ec8158c2a8215980667d7195c0325df88d
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-05-19 16:41:38 +0600

    under construction

--- src/App.js
+++ src/App.js
@@ -99,8 +99,8 @@ function App() {
 
   return (
     <>
-      {/* <Construction /> */}
-      <ProfileContext.Provider value={store}>
+      <Construction />
+      {/* <ProfileContext.Provider value={store}>
         {token ? token.length && <BottomBar /> : <></>}
         {location.pathname !== "/login" &&
           location.pathname !== "/signup" &&
@@ -112,7 +112,7 @@ function App() {
           ))}
         </Routes>
         <ToastContainer />
-      </ProfileContext.Provider>
+      </ProfileContext.Provider> */}
     </>
   );
 }
