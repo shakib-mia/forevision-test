99a2d593688079f247278c349c2a3cd6e967f96e
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-11-04 21:19:29 +0600

    fixed

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -27,30 +27,30 @@ const BottomBar = () => {
   };
 
   const navItems = [
-    {
-      icon: <FaWpforms className="text-[24px]" />,
-      text: "Forms",
-      dropdownItem: [
-        { dropdownPath: "/promotional-tool", text: "Promotional Tool" },
-        { dropdownPath: "/youtube-oac", text: "YouTube OAC" },
-        {
-          dropdownPath: "/youtube-claim-release",
-          text: "YouTube Claim Release",
-        },
-        {
-          dropdownPath: "/link-facebook-and-instagram-profile-with-songs",
-          text: "Link Facebook & Instagram Profiles with Songs",
-        },
-        {
-          dropdownPath: "/facebook-insta-whitelisting",
-          text: "Facebook & Instagram Whitelisting",
-        },
-        {
-          dropdownPath: "/youtube-video-takedown",
-          text: "YouTube Video Takedown",
-        },
-      ],
-    },
+    // {
+    //   icon: <FaWpforms className="text-[24px]" />,
+    //   text: "Forms",
+    //   dropdownItem: [
+    //     { dropdownPath: "/promotional-tool", text: "Promotional Tool" },
+    //     { dropdownPath: "/youtube-oac", text: "YouTube OAC" },
+    //     {
+    //       dropdownPath: "/youtube-claim-release",
+    //       text: "YouTube Claim Release",
+    //     },
+    //     {
+    //       dropdownPath: "/link-facebook-and-instagram-profile-with-songs",
+    //       text: "Link Facebook & Instagram Profiles with Songs",
+    //     },
+    //     {
+    //       dropdownPath: "/facebook-insta-whitelisting",
+    //       text: "Facebook & Instagram Whitelisting",
+    //     },
+    //     {
+    //       dropdownPath: "/youtube-video-takedown",
+    //       text: "YouTube Video Takedown",
+    //     },
+    //   ],
+    // },
     {
       icon: <BsUpload className="text-heading-6" />,
       text: "Song Upload",
@@ -85,7 +85,7 @@ const BottomBar = () => {
 
   return (
     <nav
-      className="fixed bottom-0 z-[99999] px-1 shadow w-screen bg-white flex justify-center xl:hidden py-1"
+      className="fixed bottom-0 z-[99999] px-1 shadow w-screen bg-white flex justify-center xl:hidden py-2"
       id="bottomBar"
     >
       {/* Left side: Home, Plans, and Hamburger */}
